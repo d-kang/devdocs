f7c16ce8dc198e28bb3aa68436961834b0d3e8b6
d-kang <david.kang714@gmail.com>
2018-04-12 12:09:35 -0700

    Add Apollo with example to App

--- graphql/src/index.jsx
+++ graphql/src/index.jsx
@@ -2,4 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: 'https://api.graph.cool/simple/v1/swapi',
+});
+
+const render = Component =>
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <Component />
+    </ApolloProvider>,
+    document.getElementById('root')
+  );
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextComponent = require('./components/App').default;
+    render(NextComponent);
+  });
+}
